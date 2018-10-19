c0a25c77daeca77c7851c18877bf724ce2fb65d1
Dmitry Kutsenko <d2emonium@gmail.com>
2018-10-19 22:35:27 +0300

    Image calculations

--- src/images-1.py
+++ src/images-1.py
@@ -4,48 +4,61 @@ from PIL import Image
 from numpy import asarray, mean, std
 
 
-def quantize(image, grade):
+def quantize(image, grade=None):
+    if grade is None:
+        return image
+
     color_delta = int(256 / grade)
+    print(grade, color_delta, int(color_delta / 2))
 
-    return image.point(lambda c: int(c / color_delta) * color_delta)
-
-
-def calculations(image, title, grade):
-    image = image.convert('L')
-    data = asarray(image)
-
-    du = data.max() - data.min()
-    l = math.log2(grade)
-    df = (2 ** 8) / (2 ** l)
-    disperse = math.sqrt((df ** 2) / 12)
-    sh = 20 * math.log10(du / disperse)
-
-    print('-' * 80)
-    print(title)
-    print("min = {}\tmax = {}".format(data.min(), data.max()))
-    print("delta = {}".format(du))
-    print("mean = {}".format(mean(data)))
-    print("std = {}".format(std(data)))
-    print("df = {}".format(df))
-    print("disperse = {}".format(disperse))
-    print("s/sh = {}".format(sh))
+    return image.point(lambda c: int(c / color_delta) * color_delta + int(color_delta / 2))
 
 
 def main(input_file, output_file):
     print("Loading image from {}...".format(input_file))
     image = Image.open(input_file).convert('L')
-    image.show()
-    calculations(image, 'original', 256)
+    # image.show()
 
-    # for grade in [8, 16, 32, 64, 128]:
-    for grade in [8,]:
-        quantified = quantize(image, grade)
-        calculations(quantified, grade, grade)
-        quantified.show(grade)
+    for grade in [None, 8, 16, 32, 64, 128]:
+        if grade is None:
+            images = image,
+        else:
+            images = image.quantize(grade), quantize(image, grade)
 
-        quantified = image.quantize(grade)
-        calculations(quantified, grade, grade)
-        quantified.show(grade)
+        print('=' * 80)
+        print("quantize grade = {}".format(grade or "original"))
+        for id, quantized in enumerate(images):
+            quantized = quantized.convert('L')
+            data = asarray(quantized)
+
+            print('-' * 20)
+            if id == 0:
+                print("PIL quantizer")
+            else:
+                print("My quantizer")
+
+            print("min = {}".format(data.min()))
+            print("max = {}".format(data.max()))
+            if grade is None:
+                print("original")
+                continue
+
+            du = data.max() - data.min()
+            L = math.log2(grade)
+            df = (2 ** 8) / (2 ** L)
+            disperse = math.sqrt((df ** 2) / 12)
+            sh = 20 * math.log10(du / disperse)
+            # K_sq =
+
+            print("delta u = {}".format(du))
+            print("mean = {}".format(mean(data)))
+            print("std = {}".format(std(data)))
+            print("df = {}".format(df))
+            print("disperse = {}".format(disperse))
+            print("s/sh = {}".format(sh))
+            # print("K sq = {}".format(K_sq))
+
+            quantized.show(grade)
 
     print(input_file, output_file)
 
@@ -54,5 +67,4 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("images-1.py <inputfile> <outputfile>")
         sys.exit(0)
-    input_file, output_file = sys.argv[1:3]
-    main(input_file, output_file)
+    main(*sys.argv[1:3])
